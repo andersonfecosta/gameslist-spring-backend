7b4b0edccbe60396d911fae612e184dada6d4e91
Anderson Costa <andersonfelipedc@gmail.com>
2023-05-09 01:09:28 -0300

    Commit inicial

---
+++ src/main/java/com/devsprintboot/gameslist/GameslistApplication.java
@@ -0,0 +1,13 @@
+package com.devsprintboot.gameslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsprintboot/gameslist/GameslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsprintboot.gameslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
