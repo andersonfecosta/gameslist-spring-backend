8f49907504d44a8574627b4c0c8930bff137788e
Anderson Costa <andersonfelipedc@gmail.com>
2023-05-11 21:43:48 -0300

    Homolog ok

--- src/main/java/com/devsprintboot/gameslist/repositories/GameRepository.java
+++ src/main/java/com/devsprintboot/gameslist/repositories/GameRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 public interface GameRepository extends JpaRepository<Games, Long> {
 
     @Query(nativeQuery = true, value = """
-		SELECT tb_games.id, tb_games.title, tb_games.game_year AS `year`, tb_games.img_url AS imgUrl,
+		SELECT tb_games.id, tb_games.title, tb_games.game_year AS gameYear, tb_games.img_url AS imgUrl,
 		tb_games.short_description AS shortDescription, tb_belonging.position
 		FROM tb_games
 		INNER JOIN tb_belonging ON tb_games.id = tb_belonging.game_id
